f3dfefd19ac54bdb0e1ac14cf4895e90de202088
NetFrame <joukejacobi@gmail.com>
2010-11-24 16:18:16 +0000

    fixes for stub

---
+++ nlp/framerules_en.py
@@ -0,0 +1,99 @@
+from frames import *
+from lexicon_en import *
+
+##########RULE DEFINITIONS ###################
+
+def false(*what, **ever): return False
+
+V = Check(pos="v")
+
+def isVNotZeg(identifier, node):
+    #TODO: niet alleen zeg-lemmata
+    pos = node.word.lemma.pos.lower()
+    if pos<> "v":
+        return False # getChild(node, "cop")
+    for lemmata in (SAY_LEMMATA, ORDER_LEMMATA):
+        if identifier.hasLemma(node, lemmata, "V"): return False
+    return True
+
+def isProposal(i, node):
+    return i.hasLemma(node, PROPOSAL_LEMMATA, "N")
+
+def isAttack(i, node):
+    return i.hasLemma(node, ATTACK_LEMMATA, "N")
+
+def isCopula(i, node):
+    if not i.hasLemma(node, COPULA_LEMMATA, "V"): return False
+    return getChild(node, "cop")
+
+def isThereIs(i, node):
+    if not isCopula(i, node): return False
+    return getChild(node, "expl", lemma="there")
+
+def allowPartialSPO(frame):
+    # Determine whether an SPO frame is allowed to be incomplete or not
+    if not frame.rule: return True # came from db
+    if frame.rule.identifier.hasLemma(frame.predicate, SUCFAIL_LEMMATA, "V"): return True
+
+def getIdentifier(db):
+    i = Identifier(db)
+    for r in [
+        BronRule(i, '1', V_SPEECH_ACTS,source=Child("nsubj"), quote=FirstMatch(Child(["dobj", "ccomp"]), Parent("dep"))),
+        BronRule(i, 'ORD', V_ORDERS, source=Child("nsubj"), addressee=Child("dobj"), quote=Serial(Child("dobj"), Child("infmod"))),
+
+        BronRule(i, 'acc', V_ACCORDING, source=Serial(Child("dep"), Child("pobj")), quote=Parent('prep')),
+        
+        #SPORule(i, "copula", predicate=Child("cop"), subject=Child("nsubj"), object=Self()),
+        #SPORule(i, "dep#", subject=Parent("dep"), object=Child("dobj"), precheck=false),
+
+        SPORule(i, "pass", object=FirstMatch(Child("nsubjpass"), Parent("partmod")), subject=FirstMatch(
+                Serial(Child("prep", lemma="by"), Child("pobj")),
+                ), precheck=isVNotZeg, allowPartial=True),
+
+        SPORule(i, 'actief',
+                precheck=isVNotZeg,
+                subject=FirstMatch(
+                  Child("nsubj"),
+                  Conditional(childOfConjoinedV, "nsubj"),
+                  Serial(Parent("xcomp"), Child("nsubj"))),#52886613
+                object=FirstMatch(
+                  #Serial(childOfLowestV("prep"), Child("pobj")),
+                  UseXtoY(),#52886613
+                  childOfLowestV("dobj"),
+                ),
+                aux = Parent("xcomp"),#52886613
+                allowPartial=allowPartialSPO),
+
+        
+        #52878501
+        SPORule(i, 'attack', precheck=isAttack, subject=Serial(Child("prep", lemma=["from"]),Child("pobj")),object=Serial(Child("prep", lemma=["on"]),Child("pobj")), allowPartial=True),
+        
+        SPORule(i, 'propose', precheck=isProposal, subject=Child("amod"), object=Serial(Child("prep", lemma="for"), Child("pobj"))),
+        
+        DeclarativeRule(i, Equal, rulename="_app", name="app", subject=Self(), object=Child("appos")),
+        DeclarativeRule(i, Equal, rulename="cop", name="cop", object=Child("cop"), subject=Child("nsubj"), predicate=Self(), precheck=isCopula),
+
+        DeclarativeRule(i, Reality, rulename="therebe", name="rea", predicate=Self(), object=Child("cop"), precheck=isThereIs),
+        
+    ]:
+        i.rules.append(r)
+    return i
+              
+
+class UseXtoY(Pattern):
+    def getNode(self, rule, node):
+        if not rule.identifier.hasLemma(node, USE_LEMMATA): return
+        node = getChild(node, "dobj")
+        if node: node=getChild(node, "infmod")
+        return node
+    
+def childOfLowestV(*args, **kargs):
+    return Child(*args, **kargs) #Serial(Lowest("aux", pos="v"), Child(*args, **kargs))
+
+def childOfConjoinedV(node, *args, **kargs):
+    node = getParent(node, "conj")
+    while node:
+        n2 = getChild(node, *args, **kargs)
+        if n2: return n2
+        node = getParent(node, "ccomp")
+    
---
+++ query/actorissuedatasource.py
@@ -0,0 +1,51 @@
+import datasource 
+import article
+import ont2
+import idlabel
+import categorise
+import lucenelib
+import dbtoolkit
+from ontologydatasource import *
+
+from amcatmetadatasource import ConceptMapper, MappedField
+
+def getActor(db, obj):
+    lbl = obj.labels.get(15)
+    if not lbl: lbl = str(obj)
+    return idlabel.IDLabel(obj.id, lbl)
+
+class ActorIssueDataSource(OntologyDataSource):
+    def __init__(self, dm, db, index):
+        artfield = MappedField(self, dm.getConcept("article"), ConceptMapper(db, article.Article))
+        issue = SetOntologyField(self, dm.getConcept("issue"), 5002, 5000, 1)
+        actor = SetOntologyField(self,dm.getConcept("actor"), 5003, 5000, 1, conceptmapper=ConceptMapper(db, getActor))
+        issuearticle = OntArtField(self, dm.getConcept("issuearticle"), issue)
+        coocissue = datasource.Field(self, dm.getConcept("coocissue"))
+        issuecooc = datasource.Field(self, dm.getConcept("issuecooc"))
+
+
+        set = SetField(self, dm.getConcept("set"))
+
+        mappings = [
+            ObjectArticleMapping(issue, artfield, db),
+            ObjectArticleMapping(actor, artfield, db),
+            ArticleOntArtMapping(artfield, issuearticle),
+            OntArtObjectMapping(issuearticle, coocissue),
+            OntArtCoocMapping(issuearticle, issuecooc),
+            SetObjectsMapping(set, issue),
+            SetObjectsMapping(set, actor),
+            ]
+        OntologyDataSource.__init__(self, dm, db, index, mappings)
+        
+if __name__ == '__main__':
+    import dbtoolkit, ont2
+    db = dbtoolkit.amcatDB()
+    ont = ont2.fromDB(db)
+
+    class Y(object): pass
+    x = Y()
+    x.ont = ont
+
+    f = HierarchyOntologyField(x, None, 16104, 10002, depth=2)
+    for o in f.getObjects():
+        print o, f.getQuery(o)

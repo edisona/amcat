7c6c59151457c89e2d9eeb39993e26bc36d840ed
Wouter van Atteveldt <wouter@vanatteveldt.com>
2011-02-23 11:59:06 +0100

    add .close, context manager, testcase

---
+++ model/article.py
@@ -0,0 +1,73 @@
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+"""
+Model module containing the Article class representing documents in the
+articles database table
+"""
+
+from __future__ import unicode_literals, print_function, absolute_import
+
+from amcat.tools.cachable.latebind import LB
+from amcat.tools.cachable.cachable import Cachable, DBProperty, ForeignKey, DBProperties
+from amcat.tools import toolkit
+
+import logging; log = logging.getLogger(__name__)
+    
+class Article(Cachable):
+    """
+    Class representing a newspaper article
+    """
+    __table__ = 'articles'
+    __idcolumn__ = 'articleid'
+    __labelprop__ = 'headline'
+    __slots__ = ()
+
+    headline, byline, metastring, section, date, length, pagenr, url, externalid, text = DBProperties(10)
+
+    project = DBProperty(LB("Project"))
+    medium = DBProperty(LB("Medium"))
+    source = medium
+    sentences = ForeignKey(LB("Sentence"))
+
+    @property
+    def fullmeta(self):
+        "@return: a dictionary representing the 'prose' metastring as a dict"
+        return toolkit.dictFromStr(self.metastring)
+
+    def fulltext(self):
+        "@return: a string containing the headline, byline, and text as paragraphs"
+        result = (self.headline or '') +"\n\n"+ (self.byline or "")+"\n\n"+(self.text or "")
+        return result.replace("\\r","").replace("\r","\n")
+
+    def getArticle(self):
+        "Convenience function also present in CodedArticle, CodedUnit"
+        return self
+
+    def words(self):
+        "@return: a generator yielding all words in all sentences"
+        for sentence in self.sentences:
+            for word in sentence.words:
+                yield word
+
+    def getSentence(self, parnr, sentnr):
+        "@return: a Sentence object with the given paragraph and sentence number"
+        for s in self.sentences:
+            if s.parnr == parnr and s.sentnr == sentnr:
+                return s
---
+++ test/test_annotationschema.py
@@ -0,0 +1,57 @@
+from amcat.test import amcattest
+
+from amcat.model.coding import codingjob, annotationschema
+from amcat.tools import idlabel
+from amcat.db import dbtoolkit
+
+
+class TestAnnotationSchema(amcattest.AmcatTestCase):
+
+    def setUp(self):
+        self.db = dbtoolkit.amcatDB(use_app=True)
+
+
+
+    def testFields(self):
+        for (sid, fieldnames) in [
+            (0, ['topic']),
+            (1, ['source', 'subject', 'predicate', 'quality', 'arrowtype', 'object', 'angle']),
+            (3, ['comments', 'irrelevant']),
+            ]:
+            # test field names
+            a = annotationschema.AnnotationSchema(self.db, sid)
+	    fields = list(a.fields)
+            fns = [f.fieldname for f in fields]
+            self.assertEqual(fns, fieldnames)
+            # test getFieldb
+            for f, fn in zip(a.fields, fieldnames):
+                f2 = a.getField(fn)
+                self.assertEqual(f, f2)
+
+    def testSchema(self):
+        # check whether we can create objects without errors
+        for (sid, name, table, articleschema) in [
+            (0, "Leeg","vw_net_arrows", False),
+            (1, "iNetSchema","vw_net_arrows", False),
+            (3, "SimpleArticleAnnotation","articles_annotations", True),
+            ]:
+            
+            a = annotationschema.AnnotationSchema(self.db, sid)
+            self.assertEqual(a.name, name)
+            self.assertEqual(a.table, table)
+            self.assertEqual(a.isarticleschema, articleschema)
+    def testSerialisation(self):
+        for (sid, fieldname, value, obj, lbl) in [
+            (1, 'arrowtype', 4, idlabel.IDLabel(4, 'AFF'), 'AFF'),
+            #TODO: (1, 'subject', 1000, ont.Object(self.db, 1000), 'ww (werkloosheidswet)'),
+            (1, 'predicate', 'bla','bla','bla'),
+            ]:
+            a = annotationschema.AnnotationSchema(self.db, sid)
+            f = a.getField(fieldname)
+            val = f.deserialize(value)
+            self.assertEqual(str(val), lbl)
+            self.assertEqual(val, obj)
+
+    
+if __name__ == '__main__':
+    amcattest.main()

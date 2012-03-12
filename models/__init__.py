cb42e4c123173e3df8c8c34c6f22120082531aa3
Wouter van Atteveldt <wouter@vanatteveldt.com>
2012-03-12 01:57:33 +0100

    improved test case for article selection

---
+++ models/__init__.py
@@ -0,0 +1,45 @@
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+from amcat.models.article import *
+from amcat.models.authorisation import *
+from amcat.models.language import *
+from amcat.models.medium import *
+from amcat.models.articleset import *
+from amcat.models.user import *
+from amcat.models.project import *
+from amcat.models.sentence import *
+from amcat.models.sentiment import *  
+
+from amcat.models.article_solr import *
+
+from amcat.models.word import *
+from amcat.models.analysis import *
+
+from amcat.models.coding.codingschema import *
+from amcat.models.coding.codingschemafield import *
+from amcat.models.coding.codingjob import *
+from amcat.models.coding.coding import *
+from amcat.models.coding.code import *
+from amcat.models.coding.codebook import *
+from amcat.models.coding.codedarticle import *
+
+from amcat.models.scraper import *
+
+

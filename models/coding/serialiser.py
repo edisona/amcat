580109623c8455dd9cb4f902d4f4be90cfd1bcd5
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2012-03-17 14:32:16 +0100

    Added scraper label, removed unused import

---
+++ models/coding/serialiser.py
@@ -0,0 +1,228 @@
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+"""
+(De)Serialisation support for coding values
+
+Coding Values need to be serialised and deserialised based on the
+schema field that they are an coding of.
+
+Definitions:
+Serialised Value: a primitive type (currently: str or int)
+Deserialised Value: a domain object, possibly a django Model instance
+
+"""
+
+import logging; log = logging.getLogger(__name__)
+
+from amcat.models.coding.code import Code, get_code
+from amcat.models.coding.codebook import get_codebook
+
+class BaseSerialiser(object):
+    """Base class for serialisation support for schema fields"""
+    def __init__(self, field, deserialised_type=str, serialised_type=str):
+        self.field = field
+        self.deserialised_type = deserialised_type
+        self.serialised_type = serialised_type
+    def deserialise(self, value):
+        """Convert the given serialised value to a domain object
+
+        @param value: The value to be deserialized, which should be of type self.serialized_type
+        @return: a value of type self.deserialized_Type. Raises an error if value could not
+                 be deserialized
+        """
+        if value is None: return None
+        return self.deserialised_type(value)
+    def serialise(self, value):
+        """Convert the given domain object to a serialised value
+
+        @param value: The value to be serialized, which should be of type self.deserialized_type
+        @return: a value of type self.serialized_Type. Raises an error if value could not
+                 be serialized
+        """
+        if value is None: return None
+        return self.serialised_type(value)
+    @property
+    def possible_values(self):
+        """Get the possible values
+
+        @return: a sequence of (deserialised) values
+                 or None if this serialiser in not for a 'drop down' field
+        """
+        return None
+
+    def value_label(self, value, language=None):
+        """Get a label for the given (deserialised) value
+
+        @param language: an optional preferred language, which may be ignored
+        """
+        return unicode(value)
+    
+class TextSerialiser(BaseSerialiser):
+    """Simple str - str serialiser"""
+    def __init__(self, field):
+        super(TextSerialiser, self).__init__(field, str, str)
+
+class IntSerialiser(BaseSerialiser):
+    """Simple int - int serialiser"""
+    def __init__(self, field):
+        super(IntSerialiser, self).__init__(field, int, int)
+
+class BooleanSerialiser(BaseSerialiser):
+    """Boolean serialiser with 'possible values'"""
+    def __init__(self, field):
+        super(BooleanSerialiser, self).__init__(self, bool, int)
+
+    @property
+    def possible_values(self):
+        return [True, False]
+        
+class QualitySerialiser(BaseSerialiser):
+    """Boolean serialiser with 'possible values'"""
+    def __init__(self, field):
+        super(QualitySerialiser, self).__init__(self, int, int)
+    @property
+    def possible_values(self):
+        return range(-10, 15, 5) # 15 because end point is ommitted
+    def value_label(self, value, language=None):
+        if value == 0: return "0"
+        return "%+1.1f" % (value / 10.)
+    
+
+_memo = {}
+def CodebookSerialiser(field):
+    """Retrieve/Create a memoized codebooksereialiser for the given field.codebook"""
+    # no harm if threads access concurrently, so no need to use local store or mutex
+    # limited total no of codebooks, so no harm in memoising all of them
+    # or should we memoise on codebook level, ie have a general codebook factory?
+    codebookid = field.codebook_id
+    try:
+        return _memo[codebookid]
+    except KeyError:
+        _memo[codebookid] = _CodebookSerialiser(field)
+        return  _memo[codebookid]
+        
+class _CodebookSerialiser(BaseSerialiser):
+    """int - amcat.models.coding.Code serialiser"""
+    def __init__(self, field):
+        super(_CodebookSerialiser, self).__init__(field, Code, int)
+        self.codebook = get_codebook(field.codebook_id)
+        
+    def deserialise(self, value):
+        try:
+            return get_code(value)
+        except ValueError:
+            # code was removed from codebook
+            return Code.objects.get(pk=value)
+            
+    def serialise(self, value):
+        if value is None: return None
+        return value.id
+    
+    @property
+    def possible_values(self):
+        return self.codebook.codes
+    
+    def value_label(self, value, language=None):
+        self.codebook.cache_labels(language)
+        return value.get_label(language)
+
+###########################################################################
+#                          U N I T   T E S T S                            #
+###########################################################################
+        
+from amcat.tools import amcattest
+
+class _DummyField(object):
+    """Dummy coding schema field object with codebook"""
+    def __init__(self, codebook):
+        self.codebook = codebook
+        self.codebook_id = codebook.id
+
+class TestSerialiser(amcattest.PolicyTestCase):
+    PYLINT_IGNORE_EXTRA = ["W0613"] # unused argument on virtual method
+    def test_textserialiser(self):
+        """Test the str serialiser"""
+        t = TextSerialiser(None)
+        self.assertEqual(t.deserialise(12), '12')
+        self.assertEqual(t.serialise('abc'), 'abc')
+        self.assertIsNone(t.possible_values)
+    def test_intserialiser(self):
+        """Test the int serialiser"""
+        t = IntSerialiser(None)
+        self.assertEqual(t.deserialise(12), 12)
+        self.assertEqual(t.serialise('-99'), -99)
+        self.assertRaises(ValueError, t.serialise, 'abc')
+        self.assertIsNone(t.possible_values)
+        
+    def test_codebookserialiser_memoisation(self):
+        """Does memoisation work?"""
+        A = amcattest.create_test_codebook(name="A")
+        B = amcattest.create_test_codebook(name="B")
+        s1 = CodebookSerialiser(_DummyField(A))
+        s2 = CodebookSerialiser(_DummyField(A))
+        self.assertIs(s1, s2)
+        s3 = CodebookSerialiser(_DummyField(B))
+        self.assertNotEqual(s2, s3)
+
+
+    def test_booleanserialiser(self):
+        """Test the boolean serialiser"""
+        b = BooleanSerialiser(None)
+        self.assertEqual({b.value_label(l) for l in b.possible_values}, {'True', 'False'})
+        self.assertEqual(b.serialise(True), 1)
+        self.assertEqual(b.deserialise(1), True)
+        self.assertEqual(b.serialise(False), 0)
+        self.assertEqual(b.deserialise(0), False)
+    
+    def test_qualityserialiser(self):
+        """Test the boolean serialiser"""
+        b = QualitySerialiser(None)
+        self.assertEqual({b.value_label(l) for l in b.possible_values},
+                         {'-1.0', '-0.5', '0' , '+0.5', '+1.0'})
+        for x in b.possible_values:
+            self.assertEqual(b.deserialise(b.serialise(x)), x)
+
+        
+    def test_codebookserialiser(self):
+        """Test the codebook serialiser"""
+        from amcat.models.language import Language
+        A = amcattest.create_test_codebook(name="A")
+        c = amcattest.create_test_code(label="bla")
+
+        l2 = Language.objects.create(label="XXX")
+        c.add_label(language=l2, label="blx")
+
+        A.add_code(c)
+        s = CodebookSerialiser(_DummyField(A))
+        self.assertEqual(s.serialise(c), c.id)
+        self.assertEqual(s.deserialise(c.id), c)
+        d = amcattest.create_test_code(label="not in codebook")
+        #(de)serialising a code from outside codebook should not raise errors:
+
+        self.assertEqual(s.serialise(d), d.id)
+        self.assertEqual(s.deserialise(d.id), d)
+
+        self.assertRaises(Exception, s.deserialise, -9999999999999999)
+        
+        self.assertEqual({c}, set(s.possible_values))
+        
+        self.assertEqual("bla", s.value_label(c))
+        self.assertEqual("blx", s.value_label(c, l2))
+        
---
+++ models/user.py
@@ -0,0 +1,205 @@
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+"""ORM Module representing users"""
+
+from __future__ import print_function, absolute_import
+import logging; log = logging.getLogger(__name__)
+import string, random
+
+from amcat.models.language import Language
+from amcat.models.project import Project
+from amcat.models import authorisation as auth
+
+from django.db import models, DEFAULT_DB_ALIAS, connections
+
+from amcat.tools.model import AmcatModel
+
+from amcat.tools import dbtoolkit
+
+
+class Affiliation(AmcatModel):
+    """
+    Model for table affiliations. Users are categorised by affiliation, and
+    some permissions are granted only for ones own  affiliation
+    """
+    __label__ = 'name'
+
+    id = models.AutoField(primary_key=True, db_column='affiliation_id')
+    name = models.CharField(max_length=200)
+    
+    class Meta():
+        db_table = 'affiliations'   
+        ordering = ['name']
+        app_label = 'amcat'
+
+    def can_update(self, user):
+        return user.haspriv('manage_users')
+
+class User(AmcatModel):
+    """
+    Model for table users. Every registered AmCAT user has one entry in the users table
+    """
+    __label__ = 'username'
+
+    id = models.AutoField(primary_key=True, db_column='user_id', editable=False)
+
+    username = models.SlugField(max_length=50, unique=True, editable=False,
+                                help_text="Only letters, digits and underscores are allowed.",
+                                db_index=True)
+
+    fullname = models.CharField(max_length=100, verbose_name="Full name")
+    active = models.BooleanField(default=True)
+    email = models.EmailField(max_length=100, unique=True)
+
+    affiliation = models.ForeignKey(Affiliation)
+    language = models.ForeignKey(Language, default=1)
+    role = models.ForeignKey(auth.Role, null=False, default=0)
+
+    def delete(self, **kwargs):
+        self.active = False
+        super(User, self).save(**kwargs)
+    
+    class Meta():
+        db_table = 'users'
+        ordering = ['username']
+        app_label = 'amcat'
+    
+    @property
+    def projects(self):
+        """Return a sequence of all projects the current user has a role in"""
+        return Project.objects.filter(projectrole__user=self)
+
+    ### Auth ###
+    def can_read(self, user):
+        return (user == self or
+                user.haspriv('view_users') or
+                (user.affiliation == self.affiliation and
+                 user.haspriv('view_users_same_affiliation')))
+
+    def can_update(self, user):
+        return (user == self or
+                user.haspriv('manage_users') or
+                (user.affiliation == self.affiliation and
+                 user.haspriv('manage_users_same_affiliation')))
+
+    @classmethod
+    def can_create(cls, user):
+        return user.haspriv('manage_users')
+
+    ### Mimic Django-functions ###
+    def set_password(self, raw_password):
+        """Set the users password to raw_password"""
+        if raw_password is None:
+            self.active = False
+        else:
+            dbtoolkit.get_database().set_password(self.username, raw_password)
+
+    def check_password(self, raw_password):
+        """Returns True iff the raw_password is correct for this user""" 
+        return dbtoolkit.get_database().check_password(self.username, raw_password)
+
+    def is_authenticated(self):
+        """Returns True iff the current user is authenticated"""
+        return True if hasattr(self, 'db') else False
+
+    def has_perm(self, perm):
+        """Returns True iff this user has this perm(ission)"""
+        return self.haspriv(perm)
+
+    def haspriv(self, privilege, onproject=None):
+        """
+        @type privilege: Privilege object, id, or str
+        @param privilege: The requested privilege
+        @param onproject: The project the privilege is requested on,
+          or None (ignored) for global privileges
+        
+        @return: True or False
+        """
+        try: auth.check(self, privilege, onproject)
+        except auth.AccessDenied:
+            return False    
+        return True
+
+    @property
+    def is_superuser(self):
+        """Returns True iff the current user is admin"""
+        return (self.role.id >= auth.ADMIN_ROLE)
+
+def current_user(using=DEFAULT_DB_ALIAS):
+    """Return the current user from the configured db"""
+    
+    username = connections.databases[using]['USER']
+    return User.objects.get(username=username)
+
+
+    
+def create_user(username, fullname, email, affiliation, language, role=None,
+		password=None, using=DEFAULT_DB_ALIAS, insert_if_db_user_exists=False):
+        """
+	Create a user with the given attributes, creating both the db user and
+	the entry in the users table
+
+	@param password: use the given password, or a random password if None
+	@param insert_if_db_user_exists: create the User object even if the user already
+	                                 exists as a database user
+        @return: A User object with the .password field set.
+	         If the db user already existed, .password will be None
+        """
+	# create and validate the User object before creating the db user
+	fields = {k:v for (k,v) in locals().items()
+		  if k in ["username","fullname","email","affiliation","language", "role"]}
+        u = User(**fields)
+	u.full_clean()
+
+	if not password: password = _random_password()
+        try:
+            dbtoolkit.get_database(using=using).create_user(username, password)
+        except dbtoolkit.UserAlreadyExists:
+            if not insert_if_db_user_exists: raise
+	    password = None
+	    
+        # Create Django user and return with .password
+        u.save()
+	u.password = password
+        return u
+
+def _random_password(length=8, chars=string.letters + string.digits):
+    #http://code.activestate.com/recipes/59873/
+    return ''.join([random.choice(chars) for i in range(length)])
+
+if __name__ == '__main__':
+    print(current_user().fullname)
+
+###########################################################################
+#                          U N I T   T E S T S                            #
+###########################################################################
+        
+from amcat.tools import amcattest
+
+class TestUser(amcattest.PolicyTestCase):
+    def test_create(self):
+        """Test whether we can create a user"""        
+        u = amcattest.create_test_user()
+        self.assertIsNotNone(u)
+
+    def test_current_user(self):
+	u = get_current_user()
+
+        

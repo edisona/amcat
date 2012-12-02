8e1643d560788213c0acb5d70c5697e29ee8a95f
Toon Alfrink <toon.alfrink@gmail.com>
2012-12-03 00:36:08 +0100

    some trivial changes to be merged with pull

---
+++ scripts/maintenance/daily.py
@@ -0,0 +1,126 @@
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+"""
+Script to be run daily for data input (scraping, preprocessing etc.
+"""
+
+from datetime import date, timedelta
+
+import logging;
+from amcat.scraping.controller import RobustController
+
+log = logging.getLogger(__name__)
+
+from django import forms
+
+from amcat.scripts.script import Script
+from amcat.scripts.maintenance.deduplicate import deduplicate_scrapers
+
+from amcat.scraping.controller import scrape_logged
+from amcat.models.scraper import get_scrapers
+
+from amcat.tools import toolkit, sendmail
+
+from amcat.tools.table import table3
+from amcat.tools.table.tableoutput import table2html
+
+MAIL_HTML = """<h3>Report for daily scraping on {datestr}</h3>
+
+<p>The following scrapers were run:</p>
+{table}
+
+<h3>Log Details</h3>
+<pre>
+{messages}
+</pre>"""
+
+
+MAIL_ASCII = MAIL_HTML
+for tag in ["h3", "p", "pre"]:
+    MAIL_ASCII = MAIL_ASCII.replace("<%s>"%tag, "").replace("</%s>"%tag, "")
+
+EMAIL = "amcat-scraping@googlegroups.com"
+
+def send_email(count, messages, date):
+    
+    t = table3.DictTable()
+    
+    for (scraper, n) in count.items():
+        t.addValue(scraper.__class__.__name__, scraper.options['date'], n)
+
+    t.rows = sorted(t.rows)
+    t.columns = reversed(sorted(t.columns))
+
+
+    n = sum(count.values())
+    tabledata_ascii = t.output(useunicode=False, box=False, rownames=True)
+    tabledata_html = table2html(t)
+    succesful = len([1 for (s,n2) in count.items() if n2>0])
+    total = len(count.items())
+
+    datestr = toolkit.writeDate(date.today())
+
+    mail_ascii = MAIL_ASCII.format(table=tabledata_ascii, **locals())
+    mail_html = MAIL_HTML.format(table=tabledata_html, **locals())
+    
+
+    subject = "Daily scraping for {datestr}: {n} articles, {succesful} out of {total} scrapers succesful".format(**locals())
+    
+    sendmail.sendmail("toon.alfrink@gmail.com", EMAIL, subject, mail_html, mail_ascii)
+
+    
+
+class DailyForm(forms.Form):
+    date = forms.DateField()
+
+class DailyScript(Script):
+    options_form = DailyForm
+
+    def run(self, _input):
+        date = self.options['date']
+
+
+        scrapers = list(get_scrapers(date=date, use_expected_articles = True))
+        
+
+        log.info("Starting scraping with {} scrapers: {}".format(
+                len(scrapers), [s.__class__.__name__ for s in scrapers]))
+        count, messages =  scrape_logged(RobustController(), scrapers, deduplicate=True)
+        
+        log.info("deduplicating...")
+        
+        deduplicate_scrapers(date)
+
+        log.info("Sending email...")
+        
+        send_email(count, messages, date)
+
+        log.info("Done")
+
+        
+        
+
+if __name__ == '__main__':
+    from amcat.tools import amcatlogging
+    from amcat.scripts.tools import cli
+    #amcatlogging.setSentryHandler()
+    amcatlogging.info_module("amcat.scraping.scraper")
+    amcatlogging.debug_module("amcat.scraping.controller")        
+    cli.run_cli(DailyScript)
---
+++ scripts/maintenance/deduplicate.py
@@ -0,0 +1,139 @@
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+
+from django import forms
+from django.db.models import Min, Max
+
+from amcat.scripts.script import Script
+from amcat.scripts.tools import cli
+
+from amcat.models.article import Article
+from amcat.models.scraper import Scraper
+from amcat.models.articleset import ArticleSet, ArticleSetArticle
+import logging; log = logging.getLogger(__name__)
+from amcat.tools import amcatlogging
+from datetime import timedelta
+
+class DeduplicateForm(forms.Form):
+    date = forms.DateField()
+    articleset = forms.ModelChoiceField(ArticleSet.objects.all())
+
+class DeduplicateScript(Script):
+    options_form = DeduplicateForm
+
+    def run(self,_input):
+        """
+        deduplicates all scraper articlesets
+        """
+        log.info("Deduplicating for articleset {articleset} at {date}".format(
+                self.options['articleset'],
+                self.options['date']))
+
+        articles = Article.objects.filter(
+            articlesetarticle__articleset = self.options['articleset'],
+            date = self.options['date']
+            )
+
+        log.info("Selected {} articles".format(len(articles)))
+
+        idDict = {}
+        for article in articles:
+            identifier = "{}{}".format(article.text,article.date)
+            if identifier:
+                if not identifier in idDict.keys():
+                    idDict[identifier] = []
+                idDict[identifier].append(article.id)
+
+        removable_ids = []
+        for ids in idDict.values():
+            if len(ids) > 1:
+                removable_ids.extend(sorted(ids)[1:])
+
+        articles.filter(id__in = removable_ids).update(project = 2) #trash project
+        ArticleSetArticle.objects.filter(article__in = removable_ids).delete()
+
+        log.info("Moved {} duplicated articles to trash".format(len(removable_ids)))
+
+
+class DeduplicatePeriodForm(forms.Form):
+    first_date = forms.DateField()
+    last_date = forms.DateField()
+    articleset = forms.ModelChoiceField(ArticleSet.objects.all())
+
+class DeduplicatePeriod(DeduplicateScript):
+    options_form = DeduplicatePeriodForm
+
+    def run(self, _input):
+        date = self.options['first_date']
+
+        while date <= self.options['last_date']:
+            self.options['date'] = date
+            super(DeduplicatePeroid, self).run(_input)
+            date += timedelta(days = 1)
+
+class DeduplicateArticlesetForm(forms.Form):
+    articleset = forms.ModelChoiceField(ArticleSet.objects.all())
+
+class DeduplicateArticleset(DeduplicatePeriod):
+    options_form = DeduplicateArticlesetForm
+    
+    def run(self, _input):
+        articles = Article.objects.filter(articlesetarticle__articleset = self.options['articleset'])
+        self.options['first_date'] = articles.aggregate(Min('date'))[1].date()
+        self.options['last_date'] = articles.aggregate(Max('date'))[1].date()
+        super(DeduplicateArticleset, self).run(_input)
+
+
+def deduplicate_scrapers(date):
+    options = {
+        'last_date':date,
+        'first_date':date - timedelta(days = 7)
+        }
+
+    scrapers = Scraper.objects.filter(run_daily='t')
+    for s in scrapers:
+        options['articleset'] = s.articleset_id
+        DeduplicatePeriod(**options).run(None)
+
+
+        
+if __name__ == '__main__':
+    amcatlogging.info_module("amcat.scripts.maintenance.deduplicate")
+    from amcat.scripts.tools import cli
+    cli.run_cli(DeduplicateScript)
+
+###########################################################################  
+#                          U N I T   T E S T S                            #  
+###########################################################################
+
+from amcat.tools import amcattest    
+
+class TestDeduplicateScript(amcattest.PolicyTestCase):
+    def test_deduplicate(self):
+        """One article should be deleted from artset and added to project 2"""
+        p = amcattest.create_test_project()
+        art1 = amcattest.create_test_article( url='blaat1', project=p)
+        art2 = amcattest.create_test_article( url='blaat2', project=p)
+        art3 = amcattest.create_test_article( url='blaat1', project=p)
+        artset = amcattest.create_test_set(articles=[art1, art2, art3])
+        d = DeduplicateScript(articleset = artset.id)
+        d.run( None )
+        self.assertEqual(len(artset.articles.all()), 2)
+        self.assertEqual(len(Article.objects.filter(project = 2)), 1)

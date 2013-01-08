cf2cebfcf83e2f05eec1e40270a022e229ded88d
Paul Huygen <paul.huygen@huygen.nl>
2013-01-08 15:31:03 +0100

    Remove dependency from dbtoolkit.py in amcat/scripts/actions/add_user.py

---
+++ amcat/nlp/sbd.py
@@ -0,0 +1,119 @@
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+"""
+Simple regex-based sentence boundary detection
+"""
+
+import re, collections
+from amcat.tools import toolkit
+
+abbrevs = ["ir","mr","dr","dhr","ing","drs","mrs","sen","sens","gov","st",
+           "jr","rev","vs","gen","adm","sr","lt","sept"]
+months = ["Jan", "Feb", "Mar", "Apr", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+from amcat.models.sentence import Sentence
+
+def get_split_regex():
+    global _split_regex
+    try:
+        return _split_regex
+    except NameError:
+        lenmap = collections.defaultdict(list)
+        for a in abbrevs+months:
+            lenmap[len(a)].append(a)
+            lenmap[len(a)].append(a.title())
+        expr = r"(?<!\b[A-Za-z])"
+        for x in lenmap.values():
+            expr += r"(?<!\b(?:%s))" % "|".join(x)
+            #expr += r"(?<Nov(?=. \d))"
+        expr += r"[\.?!](?!\.\.)(?<!\.\.)(?!\w|,)(?!\s[a-z])|\n\n"
+        expr += r"|(?<=%s)\. (?=[^\d])" % "|".join(months)
+        _split_regex = re.compile(expr)
+        return _split_regex
+
+@toolkit.to_list
+def get_or_create_sentences(article):
+    """
+    Split the given article object into sentences and save the sentences models
+    to the database. Returns a list of the resulting Sentence objects.
+
+    This function (as opposed to create_sentences) does not error when an article
+    is already split.
+    """
+    sents = Sentence.objects.filter(article=article)
+    return sents if sents.exists() else create_sentences(article)
+
+
+@toolkit.to_list # consume the generator to make sure sentences are created
+def create_sentences(article):
+    """
+    Split the given article object into sentences and save the sentences models
+    to the database. Returns a list of the resulting Sentence objects.
+    """
+    pars = [article.headline]
+    if article.byline: pars += [article.byline]
+    pars += re.split(r"\n\s*\n[\s\n]*", article.text.strip())
+    for parnr, par in enumerate(pars):
+        for sentnr, sent in enumerate(split(par)):
+            yield Sentence.objects.create(parnr=parnr+1, sentnr=sentnr+1,
+                                          article=article, sentence=sent)
+
+def split(text):
+    """
+    Split the text into sentences and yield the sentence strings
+    """
+    text = re.sub("\n\n+", "\n\n", text)
+    text = text.replace(".'", "'.")
+
+    return (re.sub('\s+', ' ', sent.strip())
+            for sent in get_split_regex().split(text)
+            if sent.strip())
+
+
+###########################################################################
+#                          U N I T   T E S T S                            #
+###########################################################################
+
+from amcat.tools import amcattest
+
+class TestSBD(amcattest.PolicyTestCase):
+    def test_split(self):
+        """Does splitting a text work correctly?"""
+        for text, sentences in [
+            ("Wat een zin! En nu s.v.p. nog een zin.",
+             ["Wat een zin", "En nu s.v.p. nog een zin"]),
+            ]:
+            result = list(split(text))
+            self.assertEqual(result, sentences)
+
+    def test_create_sentences(self):
+        hl = "This is the headline"
+        text="A sentence.\n\nAnother sentence. And yet a third"
+        a = amcattest.create_test_article(headline=hl, text=text)
+        create_sentences(a)
+        sents = Sentence.objects.filter(article=a.id)
+        sents = set((s.parnr, s.sentnr, s.sentence) for s in sents)
+        self.assertEqual(sents, {(1,1,hl),
+                                 (2,1,"A sentence"),
+                                 (3,1,"Another sentence"),
+                                 (3,2,"And yet a third")})
+
+                                          
+        
---
+++ amcat/scripts/forms.py
@@ -0,0 +1,202 @@
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+
+from django import forms
+from amcat.models.project import Project
+from amcat.models.articleset import ArticleSet
+from amcat.models.medium import Medium
+
+import logging
+log = logging.getLogger(__name__)
+
+
+class ModelMultipleChoiceFieldWithIdLabel(forms.ModelMultipleChoiceField):
+    def label_from_instance(self, obj):
+        return "%s - %s" % (obj.id, obj.name)
+        
+class ModelChoiceFieldWithIdLabel(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return "%s - %s" % (obj.id, obj.name)
+        
+        
+class DateIntervalForm(forms.Form):
+    interval = forms.ChoiceField(
+            choices=(
+                ('day', 'Day'), 
+                ('week', 'Week'), 
+                ('month', 'Month'), 
+                ('quarter', 'Quarter'), 
+                ('year', 'Year')
+            ), initial='month')
+        
+class InlineTableOutputForm(forms.Form):
+    """Form with all the possible outputs for a table3 object"""
+    output = forms.ChoiceField(choices=(
+        ('csv', 'CSV (semicolon separated)'),
+        ('comma-csv', 'CSV (comma separated)'),
+        ('excel', 'Excel (.xslx)'),
+        ('spss', 'SPSS (.sav)'), 
+        ('json-html', 'Inline HTML')
+     ), initial='json-html')
+        
+class TableOutputForm(forms.Form):
+    """Form with all the possible outputs for a table3 object"""
+    output = forms.ChoiceField(choices=(
+        ('csv', 'CSV (semicolon separated)'),
+        ('comma-csv', 'CSV (comma separated)'),
+        ('excel', 'Excel (.xslx)'),
+        ('spss', 'SPSS (.sav)'), 
+        ('html', 'HTML')
+     ), initial='csv')
+        
+        
+class GeneralColumnsForm(forms.Form):
+    """represents column for any object, as a string seperated with ,"""
+    columns = forms.CharField()
+
+    def clean_columns(self):
+        data = self.cleaned_data['columns']
+        data = [x.strip() for x in data.split(',') if x.strip()]
+        return data
+        
+        
+class ArticleColumnsForm(forms.Form):
+    columns = forms.MultipleChoiceField( # columns are used to indicate which columns should be loaded from the database (for performance reasons)
+            choices=(
+                ('article_id', 'Article ID'),
+                ('hits', 'Hits'),
+                ('keywordInContext', 'Keyword in Context'),
+                ('date','Date'),
+                ('interval', 'Interval'),
+                ('medium_id','Medium ID'),
+                ('medium_name','Medium Name'),
+                ('project_id','Project ID'),
+                ('project_name','Project Name'),
+                ('pagenr','Page number'),
+                ('section','Section'),
+                ('author','Author'),
+                ('length','Length'),
+                ('url','url'),
+                ('parent_id','Parent Article ID'),
+                ('externalid','External ID'),
+                ('additionalMetadata','Additional Metadata'),
+                ('headline','Headline'),
+                ('byline','Byline'),
+                ('text','Article Text')
+            ), initial = ('article_id', 'date', 'medium_id', 'medium_name', 'headline')
+    )
+    columnInterval = forms.ChoiceField(
+            choices=(
+                ('day', 'Day'), 
+                ('week', 'Week'), 
+                ('month', 'Month'), 
+                ('quarter', 'Quarter'), 
+                ('year', 'Year')
+            ), initial='month', label='Column Interval', required=False)
+
+            
+            
+class SearchQuery(object):
+    """
+    represents a query object that contains both a (Solr) query and an optional label
+    """
+    def __init__(self, query, label=None):
+        self.query = query
+        self.label = label or query
+            
+            
+class SelectionForm(forms.Form):
+    # TODO: change to projects of user
+    projects = ModelMultipleChoiceFieldWithIdLabel(queryset=Project.objects.order_by('-pk')) 
+    articlesets = ModelMultipleChoiceFieldWithIdLabel(queryset=ArticleSet.objects.none(), required=False)
+    mediums = ModelMultipleChoiceFieldWithIdLabel(queryset=Medium.objects.none(), required=False)
+    query = forms.CharField(widget=forms.Textarea, required=False)
+    articleids = forms.CharField(widget=forms.Textarea, required=False)
+    datetype = forms.ChoiceField(choices=(('all', 'All Dates'), ('before', 'Before'), ('after', 'After'),
+                                          ('between', 'Between')), initial='all')
+    startDate = forms.DateField(input_formats=('%d-%m-%Y',), required=False)
+    endDate = forms.DateField(input_formats=('%d-%m-%Y',), required=False)
+    # queries will be added by clean(), that contains a list of SearchQuery objects
+    
+    def __init__(self, data=None, *args, **kwargs):
+        super(SelectionForm, self).__init__(data, *args, **kwargs)
+        if data is None: return
+
+        projectids = data.getlist('projects') if hasattr(data, 'getlist') else data.get('projects')
+        if type(projectids) != list:
+            return
+        projectids = map(int, projectids)
+        
+        self.fields['articlesets'].queryset = ArticleSet.objects.order_by('-pk').filter(project__in=projectids)
+        self.fields['mediums'].queryset = Medium.objects.all().order_by('pk')
+
+        
+    def clean(self):
+        cleanedData = self.cleaned_data
+        if cleanedData.get('datetype') in ('after', 'all') and 'endDate' in cleanedData:
+            del cleanedData['endDate']
+        if cleanedData.get('datetype') in ('before', 'all') and 'startDate' in cleanedData:
+            del cleanedData['startDate']
+        missingDateMsg = "Missing date"
+        if 'endDate' in cleanedData and cleanedData['endDate'] == None: # if datetype in (before, between)
+            self._errors["endDate"] = self.error_class([missingDateMsg])
+            del cleanedData['endDate']
+            #raise forms.ValidationError("Missing end date")
+        if 'startDate' in cleanedData and cleanedData['startDate'] == None: # if datetype in (after, between)
+            self._errors["startDate"] = self.error_class([missingDateMsg])
+            del cleanedData['startDate']
+            #raise forms.ValidationError("Missing start date")
+        if cleanedData.get('query') == '':
+            del cleanedData['query']
+            cleanedData['useSolr'] = False
+        else:
+            cleanedData['useSolr'] = True
+            cleanedData['queries'] = []
+            queries = [x.strip() for x in cleanedData['query'].split('\n') if x.strip()] # split lines
+            for query in queries:
+                if '#' in query:
+                    label = query.split('#')[0]
+                    if len(label) == 0 or len(label) > 20:
+                        self._errors["query"] = self.error_class(['Invalid query label (before the #)'])
+                    query = query.split('#')[1]
+                    if len(query) == 0:
+                        self._errors["query"] = self.error_class(['Invalid query (after the #)'])
+                elif '\t' in query:
+                    label = query.split('\t')[0]
+                    if len(label) == 0 or len(label) > 20:
+                        self._errors["query"] = self.error_class(['Invalid query label (before the tab)'])
+                    query = query.split('\t')[1]
+                    if len(query) == 0:
+                        self._errors["query"] = self.error_class(['Invalid query (after the tab)'])
+                else: 
+                    label = None
+                if '[' in query:
+                    for query2 in cleanedData['queries']:
+                        query = query.replace('[%s]' % query2.label, '(%s)' % query2.query)
+                cleanedData['queries'].append(SearchQuery(query, label))
+            
+        try:
+            cleanedData['articleids'] = [int(x.strip()) for x in cleanedData['articleids'].split('\n') if x.strip()]
+        except:
+            self._errors["articleids"] = self.error_class(['Invalid article ID list'])
+        # if 'output' not in cleanedData:
+            # cleanedData['output'] = 'json-html'
+            
+        return cleanedData

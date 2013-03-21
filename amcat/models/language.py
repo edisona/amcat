a03fb80e6638ad4f45578f17a6d6261fba725c51
Toon Alfrink <toon.alfrink@gmail.com>
2013-03-21 11:29:54 +0100

    bugfix generate_expected_articles

---
+++ amcat/models/language.py
@@ -0,0 +1,11 @@
+from amcat.tools.model import AmcatModel
+
+from django.db import models
+
+class Language(AmcatModel):    
+    id = models.AutoField(primary_key=True, db_column="language_id")
+    label = models.CharField(max_length=50, unique=True)
+
+    class Meta():
+        db_table = 'languages'
+        app_label = 'amcat'

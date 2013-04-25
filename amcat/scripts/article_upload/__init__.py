137ba926d009f2151c58077d228eef4c4be44ea4
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2013-04-25 14:56:49 +0200

    Set default ordering to -id for datatables

---
+++ amcat/scripts/article_upload/__init__.py
@@ -0,0 +1,6 @@
+from amcat.scripts.article_upload.lexisnexis import *
+from amcat.scripts.article_upload.text import *
+from amcat.scripts.article_upload.csv_import import *
+from amcat.scripts.article_upload.mediargus import *
+
+__all__ = ['LexisNexis', 'Text', 'CSV', 'Mediargus']

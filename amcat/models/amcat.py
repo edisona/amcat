616a77feb8ee358c9974b071b9e58553a3e87bb5
Wouter van Atteveldt <wouter@vanatteveldt.com>
2013-05-10 16:34:00 +0300

    added zipfile support to article upload

---
+++ amcat/models/amcat.py
@@ -0,0 +1,77 @@
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+from __future__ import unicode_literals, print_function, absolute_import
+
+from django.db import models
+from amcat.tools.model import AmcatModel
+import os
+import logging;
+log = logging.getLogger(__name__)
+
+WARNING_NOT_PRODUCTION = ("This is {server}. "
+                           "Use <a href='http://amcat.vu.nl'>amcat.vu.nl</a> "
+                           "unless you explicitly want to use this server.")
+
+class AmCAT(AmcatModel):
+    id = models.BooleanField(primary_key=True, db_column="singleton_pk")
+
+    def save(self, *args, **kwargs):
+        raise NotImplementedError()
+    
+    global_announcement = models.TextField(blank=True, null=True)
+
+    @property
+    def server_warning(self):
+        status = os.environ.get('AMCAT_SERVER_STATUS', '')
+        if status != "production":
+            if status:
+                server = "the {status} server".format(**locals())
+            else:
+                server = "not the production server"
+            return WARNING_NOT_PRODUCTION.format(**locals())
+
+    @classmethod
+    def get_instance(cls):
+        return cls.objects.get(pk=1)
+
+
+    class Meta():
+        db_table = 'amcat_system'
+        app_label = 'amcat'
+
+
+###########################################################################
+#                          U N I T   T E S T S                            #
+###########################################################################
+
+from amcat.tools import amcattest
+
+class TestAmCAT(amcattest.PolicyTestCase):
+    def test_get_instance(self):
+        a = AmCAT.get_instance()
+        self.assertEqual(type(a), AmCAT)
+
+        os.environ['AMCAT_SERVER_STATUS']=""
+        self.assertEqual(a.server_warning,
+                         ANNOUNCE_NOT_PRODUCTION.format(server = "not the production server"))
+
+        os.environ['AMCAT_SERVER_STATUS']="production"
+        self.assertEqual(a.server_warning, None)
+

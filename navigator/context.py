2a1a4c02d36fe058aa6a37489b59059d62a6ccaf
Wouter van Atteveldt <wouter@vanatteveldt.com>
2013-05-26 14:13:33 +0200

    fixed announcement quotes^C

---
+++ navigator/context.py
@@ -0,0 +1,27 @@
+from amcat.models import AmCAT
+
+DISPLAY_COUNT = 3
+ANNOUNCE_KEY = "last_announcement"
+COUNT_KEY = "last_announcement_count"
+
+# Extra context variables
+def extra(request):
+    announcement = AmCAT.get_instance().global_announcement
+    last_announcement = request.session.get(ANNOUNCE_KEY)
+    count = int(request.session.get(COUNT_KEY, 0)) + 1
+
+    print ">>> extra", [announcement, last_announcement, count]
+
+    if last_announcement == announcement and count >= DISPLAY_COUNT:
+        announcement = None
+    elif last_announcement != announcement:
+        request.session["last_announcement"] = announcement
+        count = 0
+
+    if count < DISPLAY_COUNT:
+        request.session[COUNT_KEY] = count
+
+    if announcement:
+        print "!!! Sending announcement", `announcement`
+
+    return dict(request=request, warning=AmCAT.get_instance().server_warning, announcement=announcement)

ba9703e5a8c352a0f21efd1f6eb919d76a150c11
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2013-07-03 10:50:21 +0200

    codebookcodes now uses cache

---
+++ amcat/tests/test_selection.py
@@ -0,0 +1,102 @@
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+"""
+Testing module for the article selection scripts
+"""
+
+from __future__ import unicode_literals, print_function, absolute_import
+
+from amcat.tools import amcattest
+
+from amcat.scripts.searchscripts.articlelist import ArticleListScript
+from amcat.scripts.searchscripts.aggregation import AggregationScript, AggregationForm
+
+from django.utils.datastructures import MultiValueDict
+
+        
+DEFAULTS = dict(datetype="all", columns=['article_id'], yAxis='total', counterType='numberOfArticles')
+
+class TestArticleList(amcattest.PolicyTestCase):
+
+    def list(self, **kargs):
+        options = DEFAULTS.copy()
+        options.update(kargs)
+        return set(ArticleListScript(**options).run())
+
+    def aggr(self, **kargs):
+        options = DEFAULTS.copy()
+        options.update(kargs)
+        return list(AggregationScript(**options).run().to_list(tuple_name=None))
+    
+    def test_selection(self):
+        """Can we select articles outside the project?"""
+
+        # baseline: can we select articles in a project
+        p = amcattest.create_test_project()
+        arts = {amcattest.create_test_article(project=p) for i in range(10)}
+        amcattest.create_test_set(project=p).add_articles(arts)
+        self.assertEqual(self.list(projects=[p.id]), arts)
+
+        # add second project with articles from first project in set
+        p2 = amcattest.create_test_project()
+        s = amcattest.create_test_set(project=p2)
+        s.add(*arts)
+        # selecting on only project OR on project and set should give the articles
+        self.assertEqual(self.list(projects=[p2.id]), arts)
+        self.assertEqual(self.list(projects=[p2.id], articlesets=[s.id]), arts)
+
+    def test_aggregation(self):
+        """Can we create nice tables?"""
+        p = amcattest.create_test_project()
+        m1, m2 = [amcattest.create_test_medium() for x in [1,2]]
+        arts1 = {amcattest.create_test_article(project=p, medium=m1) for i in range(5)}
+        arts2 = {amcattest.create_test_article(project=p, medium=m2) for i in range(15)}
+        amcattest.create_test_set(project=p).add_articles(arts1|arts2)
+        # can we select on mediumid
+        self.assertEqual(self.list(projects=[p.id]), arts1|arts2)
+        self.assertEqual(self.list(projects=[p.id], mediums=[m1.id]), arts1)
+
+        # can we make a table?
+        x = self.aggr(projects=[p.id], xAxis='medium')
+        self.assertEqual(set(x), {(5,), (15,)})
+
+        
+        # add second project with articles from first project in set
+        p2 = amcattest.create_test_project()
+        s = amcattest.create_test_set(project=p2)
+        s.add(*(arts1|arts2))
+        x = self.aggr(projects=[p2.id], articlesets=[s.id], xAxis='medium')
+
+    def test_form(self):
+        p = amcattest.create_test_project()
+        arts = {amcattest.create_test_article(project=p) for i in range(10)}
+        form = dict(yAxis=['medium'], counterType=['numberOfArticles'], articlesets=[],
+                    xAxis=[u'date'], datetype=['all'], projects=[unicode(p.id)])
+        
+        #f = AggregationForm(data=MultiValueDict(form))
+        #valid = f.is_valid()
+        #self.assertTrue(valid, "Validation errors: {f.errors}".format(**locals()))
+
+        s = amcattest.create_test_set(project=p)
+        form['articlesets'] = [unicode(s.id)]
+        f = AggregationForm(data=MultiValueDict(form))
+        valid = f.is_valid()
+        self.assertTrue(valid, "Validation errors: {f.errors}".format(**locals()))
+        
---
+++ navigator/views/scriptview.py
@@ -0,0 +1,120 @@
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+from django.views.generic.edit import FormMixin, ProcessFormView
+from django.views.generic.base import TemplateResponseMixin
+
+from django.forms.widgets import HiddenInput
+
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse
+from django import forms
+
+from navigator.utils.auth import check
+from amcat.models import Project
+from amcat.tools.table import table3
+
+class ScriptMixin(FormMixin):
+    script = None # plugin/script to base the view on
+
+    def get_script(self):
+        return self.script
+    
+    def get_form_class(self):
+        return self.get_script().options_form
+    
+    def form_valid(self, form):
+        self.form = form
+        self.result = self.get_script().run_script(form)
+        return super(ScriptMixin, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(ScriptMixin, self).get_context_data(**kwargs)
+        context['script'] = self.get_script()
+        context['script_name'] = self.get_script().name # for some reason {{ script.name }} in a template instantiates script...
+        return context
+    
+
+class ScriptView(ProcessFormView, ScriptMixin, TemplateResponseMixin):
+    pass
+
+class ProjectScriptView(ScriptView):
+    """
+    View that provides access to a Script from within a plugin.
+    Subclasses should provide a script instance.
+    """
+    template_name = "navigator/project/script_base.html"
+
+    def get_initial(self):
+        initial = super(ProjectScriptView, self).get_initial()
+        initial["project"] = self.project
+        return initial
+        
+    def get_success_url(self):
+        return reverse("project", kwargs=dict(id=self.project.id))
+        
+    def get_form(self, form_class):
+        form = super(ScriptView, self).get_form(form_class)
+        if self.request.method == 'GET':
+            for key, val in self.url_data.iteritems():
+                if key in form.fields:
+                    form.fields[key].initial = val
+        if 'project' in form.fields:
+            form.fields['project'].widget = HiddenInput()
+        return form
+
+    def _initialize_url_data(self, **kwargs):
+        self.project = Project.objects.get(pk=kwargs["projectid"])
+        self.url_data = kwargs
+    
+    def post(self, request, **kwargs):
+        self._initialize_url_data(**kwargs)
+        return super(ProjectScriptView, self).post(request, **kwargs)
+                
+    def get(self, request, **kwargs):
+        self._initialize_url_data(**kwargs)
+        return super(ProjectScriptView, self).get(request, **kwargs)
+        
+    def get_context_data(self, **kwargs):
+        context = super(ProjectScriptView, self).get_context_data(**kwargs)
+        context["project"] = self.project
+        context["script_doc"] = self.script.__doc__.strip()
+        return context
+
+class TableExportMixin():
+
+    def export_filename(self, form):
+        """Return the filename to export as (without extension)"""
+        return self.__class__.__name__
+        
+    def get_form_class(self):
+        
+        class ExportFormWrapper(self.script.options_form):
+            format = forms.ChoiceField(choices = [(k, v.name) for (k,v) in table3.EXPORTERS.items()],
+                                       label = "Export format")
+        return ExportFormWrapper
+        
+    def form_valid(self, form):
+        table = self.get_script().run_script(form)
+        exporter = table3.EXPORTERS[form.cleaned_data["format"]]
+        filename = "{fn}.{exporter.extension}".format(fn=self.export_filename(form), **locals())
+        response = HttpResponse(content_type='text/csv', status=200)
+        response['Content-Disposition'] = 'attachment; filename="{filename}"'.format(**locals())
+        exporter.export(table, stream = response)
+        return response

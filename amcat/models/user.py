734f673950e188a60d893fa64ded8fc7b17e6b68
Wouter van Atteveldt <wouter@vanatteveldt.com>
2013-10-12 00:17:49 +0200

    prevent deduplicate cycle

---
+++ amcat/models/user.py
@@ -0,0 +1,176 @@
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+"""ORM Module representing users"""
+
+from __future__ import print_function, absolute_import
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from amcat.models.language import Language
+from amcat.models.authorisation import Role
+from amcat.models import authorisation as auth
+from amcat.models.project import Project
+
+import logging;
+
+log = logging.getLogger(__name__)
+
+import string, random
+
+from django.db import models
+from amcat.tools.model import AmcatModel
+LITTER_USER_ID = 1
+
+
+class Affiliation(AmcatModel):
+    """
+    Model for table affiliations. Users are categorised by affiliation, and
+    some permissions are granted only for ones own  affiliation
+    """
+    __label__ = 'name'
+
+    id = models.AutoField(primary_key=True, db_column='affiliation_id')
+    name = models.CharField(max_length=200)
+
+    class Meta():
+        db_table = 'affiliations'
+        ordering = ('name',)
+        app_label = 'amcat'
+
+    def can_update(self, user):
+        return user.haspriv('manage_users')
+
+class UserProfile(AmcatModel):
+    """
+    Additional user information is stored here
+    """
+    user = models.OneToOneField(User)
+
+    affiliation = models.ForeignKey(Affiliation, default=1)
+    language = models.ForeignKey(Language, default=1)
+    role = models.ForeignKey(Role, default=0)
+
+    favourite_projects = models.ManyToManyField("amcat.project", related_name="favourite_users")
+    
+    @property
+    def projects(self):
+        return Project.objects.filter(projectrole__user=self.user)
+
+    def haspriv(self, privilege, onproject=None):
+        """
+        @type privilege: Privilege object, id, or str
+        @param privilege: The requested privilege
+        @param onproject: The project the privilege is requested on,
+          or None (ignored) for global privileges
+
+        @return: True or False
+        """
+        try: auth.check(self.user, privilege, onproject)
+        except auth.AccessDenied:
+            return False
+        return True
+
+    def can_read(self, user):
+        profile = user.get_profile()
+
+        return (self == profile) or\
+               (profile.affiliation == self.affiliation and\
+                profile.haspriv("view_users_same_affiliation")) or\
+               (profile.haspriv("view_users"))
+
+    def can_update(self, user):
+        profile = user.get_profile()
+
+        return (self == profile) or\
+               (profile.affiliation == self.affiliation and\
+                profile.haspriv("manage_users_same_affiliation")) or\
+               (profile.haspriv("manage_users"))
+
+    def can_delete(self, user):
+        return False
+
+    def save(self, force_permissions=False, **kwargs):
+        if not force_permissions:
+            return super(UserProfile, self).save(**kwargs)
+
+        # TODO / CHALLANGE: find the solution that doesn't skip a superclass
+        return super(AmcatModel, self).save(**kwargs)
+
+        
+
+    class Meta():
+        db_table = 'auth_user_profile'
+        app_label = "amcat"
+
+def create_user(username, first_name, last_name, email, affiliation, language, role,
+                password=None):
+        """
+        Create a user with the given attributes, creating both the db user and
+        the entry in the users table.
+
+        @param password: use the given password, or a random password if None
+        @return: A User object with the .password field set.
+                 If the db user already existed, full_clean will raise an error
+        """
+        # create and validate the User object before creating the db user
+        fields = dict((k, v) for (k,v) in locals().items()
+                  if k in ("username","first_name", "last_name", "email"))
+
+        # Create user
+        password = password or _random_password()
+
+        u = User.objects.create_user(username, email, password)
+        u.first_name = first_name
+        u.last_name = last_name
+        u.save()
+
+        # Correct profile
+        prof = u.get_profile()
+        prof.role = role
+        prof.affiliation = affiliation
+        prof.language = language
+        prof.save(force_permissions=True)
+
+        return u
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    """
+    This function is executed when a Django User model is created.
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+
+post_save.connect(create_user_profile, sender=User, dispatch_uid="create_user_profile")
+
+def _random_password(length=8, chars=string.letters + string.digits):
+    #http://code.activestate.com/recipes/59873/
+    return ''.join([random.choice(chars) for i in range(length)])
+
+###########################################################################
+#                          U N I T   T E S T S                            #
+###########################################################################
+
+from amcat.tools import amcattest
+
+class TestUser(amcattest.PolicyTestCase):
+    def test_create(self):
+        """Test whether we can create a user"""
+        u = amcattest.create_test_user()
+        self.assertIsNotNone(u)
---
+++ amcat/scripts/article_upload/csv_import.py
@@ -0,0 +1,304 @@
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+"""
+Plugin for uploading csv files
+"""
+
+
+
+from __future__ import unicode_literals, absolute_import
+
+import csv
+from cStringIO import StringIO
+from types import NoneType
+
+from django import forms
+
+from amcat.scripts.article_upload.upload import UploadScript
+from amcat.scripts.article_upload import fileupload
+
+from amcat.models.article import Article
+from amcat.models.medium import Medium
+
+from amcat.tools.toolkit import readDate
+
+FIELDS = ("text", "date", "medium", "pagenr", "section", "headline", "byline",  "url", "externalid",
+          "author", "addressee", "parent_url", "parent_externalid")
+REQUIRED = [True] * 2 + [False] * (len(FIELDS) - 2)
+
+PARSERS = {
+    "date" : readDate,
+    "pagenr" : int,
+    "externalid" : int,
+    "parent_externalid" : int,
+    "medium" : Medium.get_or_create
+}
+
+HELP_TEXTS = {
+    "parent_url" : "Column name for the URL of the parent article, which should be in the same CSV file",
+    "parent_externalid" : "Column name for the External ID of the parent article, which should be in the same CSV file",
+    }
+
+def is_nullable(field_name):
+    return Article._meta.get_field(field_name).null
+
+class CSVForm(UploadScript.options_form, fileupload.CSVUploadForm):
+    medium_name = forms.CharField(
+        max_length=Article._meta.get_field_by_name('medium')[0].max_length,
+        required = False)
+    medium_existing = forms.ModelChoiceField(
+        queryset=Medium.objects.all(), required=False,
+        help_text="Use this option if you want to choose one medium for all uploaded articles."
+    )
+
+    addressee_from_parent = forms.BooleanField(required=False, initial=False, label="Addressee from parent",
+                                               help_text="If set, will set the addressee field to the author of the parent article")
+    
+    def clean_medium_name(self):
+        cd = self.cleaned_data
+        name = self.cleaned_data['medium_name']
+
+        if not (name or ("medium" in self.data and self.data["medium"]) or "medium_existing" in cd):
+            raise forms.ValidationError("Please specify either medium, medium_existing or medium_name")
+
+        return name
+    
+    def __init__(self, *args, **kargs):
+        super(CSVForm, self).__init__(*args, **kargs)
+        for fieldname, required in reversed(zip(FIELDS, REQUIRED)):
+            label = fieldname + " field"
+            if fieldname in HELP_TEXTS:
+                help_text = HELP_TEXTS[fieldname]
+            else:
+                help_text = "Column name for the article {}".format(fieldname)
+                if not required:
+                    help_text += ", or leave blank to leave unspecified"
+
+            initial = fieldname if required else None
+    
+            field = forms.CharField(help_text = help_text, required=required,
+                                    initial=initial, label=label)
+            self.fields.insert(7, fieldname, field)
+    
+    
+    def clean_parent_url(self):
+        idfield = self.cleaned_data['parent_url']
+        if idfield and self.cleaned_data['parent_externalid']:
+            raise forms.ValidationError("Cannot specify both external id and URL for parents")
+        return idfield
+        
+
+
+class CSV(UploadScript):
+    options_form = CSVForm
+
+    def explain_error(self, error):
+        if isinstance(error.error, KeyError):
+            return "Field {error.error} not found in row {error.i}. Check field name and/or csv dialect".format(**locals())
+        return super(CSV, self).explain_error(error)
+    
+    def run(self, *args, **kargs):
+
+        if self.options['parent_url']:
+            self.id_field, self.parent_field = 'url', 'parent_url'
+        elif self.options['parent_externalid']:
+            self.id_field, self.parent_field = 'externalid', 'parent_externalid'
+        else:
+            self.id_field, self.parent_field = None, None
+            
+        if self.parent_field:
+            self.parents = {} # id/url : id/url
+            self.articles = {} # id/url : article
+        
+        return super(CSV, self).run(*args, **kargs)
+    
+    @property
+    def _medium(self):
+        if self.options["medium"]:
+            return
+
+        if self.options['medium_existing']:
+            return self.options['medium_existing']
+
+        if self.options['medium_name']:
+            med = Medium.get_or_create(self.options['medium_name'])
+            self.options['medium_existing'] = med
+            return med
+
+        raise ValueError("No medium specified!")
+    
+    def parse_document(self, row):
+        kargs = dict(medium = self._medium)
+        for fieldname in FIELDS:
+            csvfield = self.options[fieldname]
+            if not csvfield: continue
+            val = row[csvfield]
+            if val.strip():
+                if fieldname in PARSERS:
+                    val = PARSERS[fieldname](val)
+            elif is_nullable(fieldname):
+                val = None
+            else:
+                val = val.strip()
+                
+            kargs[fieldname] = val
+
+        # In case medium wasn't defined in csv
+        medium = self._medium
+        if medium is not None:
+            kargs["medium"] = medium
+
+        if self.parent_field:
+            doc_id = kargs.get(self.id_field)
+            parent_id = kargs.pop(self.parent_field)
+            if parent_id:
+                self.parents[doc_id] = parent_id
+            
+        article = Article(**kargs)
+        if self.parent_field:
+            self.articles[doc_id] = article
+
+        return article
+
+    def postprocess(self, articles):
+        if self.parent_field:
+            for doc_id, parent_id in self.parents.iteritems():
+                doc = self.articles[doc_id]
+                doc.parent = self.articles[parent_id]
+                if not doc.addressee and self.options['addressee_from_parent']:
+                    doc.addressee = doc.parent.author
+                
+                doc.save()
+
+    
+if __name__ == '__main__':
+    from amcat.scripts.tools import cli
+    cli.run_cli(CSV)
+
+###########################################################################
+#                          U N I T   T E S T S                            #
+###########################################################################
+
+from amcat.tools import amcattest
+
+
+
+def _run_test_csv(header, rows, **options):
+    p = amcattest.create_test_project()
+    from tempfile import NamedTemporaryFile
+    from django.core.files import File
+    with NamedTemporaryFile(suffix=".txt") as f:
+        w = csv.writer(f)
+        for row in [header] + list(rows):
+            w.writerow([field and field.encode('utf-8') for field in row])
+        f.flush()
+
+        return CSV(dict(file=File(open(f.name)), encoding=0, project=p.id,
+                        medium_name=options.pop("medium_name", 'testmedium'), **options)).run()
+
+class TestCSV(amcattest.PolicyTestCase):
+    
+    def test_csv(self):
+        header = ('kop', 'datum', 'tekst', 'pagina')
+        data = [('kop1', '2001-01-01', 'text1', '12'), ('kop2', '10 maart 1980', 'text2', None)]
+        articles = _run_test_csv(header, data, text="tekst", headline="kop", date="datum", pagenr='pagina')
+        self.assertEqual(len(articles), 2)
+        self.assertEqual(articles[0].headline, 'kop1')
+        self.assertEqual(articles[0].pagenr, 12)
+        self.assertEqual(articles[1].date.isoformat()[:10], '1980-03-10')
+        self.assertEqual(articles[1].pagenr, None)
+
+    def test_text(self):
+        header = ('kop', 'datum', 'tekst')
+        data = [('kop1', '2001-01-01', '')]
+        articles = _run_test_csv(header, data, text="tekst", headline="kop", date="datum")
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].text, "")
+
+    def test_medium(self):
+        import functools
+        header = ('kop', 'datum', 'tekst', 'med')
+        data = [('kop1', '2001-01-01', '', 'Bla')]
+
+        test = functools.partial(_run_test_csv, header, data, text="tekst", headline="kop", date="datum")
+        articles = test(medium_name=None, medium="med")
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].medium.name, "Bla")
+
+        articles = test(medium_existing=Medium.get_or_create("1").id)
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].medium.name, "1")
+
+        articles = test(medium_existing=Medium.get_or_create("1").id, medium="med")
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].medium.name, "Bla")
+
+        articles = test(medium_name="bla2", medium="med")
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].medium.name, "Bla")
+
+        articles = test(medium_name="bla2", medium_existing=Medium.get_or_create("2").id)
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].medium.name, "2")
+
+    def test_parents(self):
+        header = ('kop', 'datum', 'tekst', 'id', 'parent', 'van')
+        data = [('kop1', '2001-01-01', 'text1', "7", "12", 'piet'), ('kop1', '2001-01-01', 'text1', "12", None, 'jan')]
+        articles = _run_test_csv(header, data, text="tekst", headline="kop", date="datum",
+                                 externalid='id',  parent_externalid='parent', author='van')
+        
+        self.assertEqual(len(articles), 2)
+        self.assertEqual(articles[0].parent, articles[1])
+        self.assertEqual(articles[0].externalid, 7)
+        self.assertEqual(articles[0].author, 'piet')
+        self.assertEqual(articles[0].addressee, None)
+
+        
+        self.assertEqual(articles[1].parent, None)
+        self.assertEqual(articles[1].externalid, 12)
+        self.assertEqual(articles[1].author, 'jan')
+        self.assertEqual(articles[1].addressee, None)
+
+        
+        articles = _run_test_csv(header, data, text="tekst", headline="kop", date="datum",
+                                 externalid='id',  parent_externalid='parent', author='van',
+                                 addressee_from_parent=True)
+
+        self.assertEqual(articles[0].author, 'piet')
+        self.assertEqual(articles[0].addressee, 'jan')
+        self.assertEqual(articles[1].author, 'jan')
+        self.assertEqual(articles[1].addressee, None)
+                
+        
+
+    def test_date_format(self):
+        # Stump class to test future 'date format' option, if needed. Currently just checks that
+        # a variety of formats load correctly. 
+        header = "date", "text"
+
+        for datestr, dateformat, expected in [
+            ("2001-01-01", None, "2001-01-01"),
+            ("10/03/80", None, "1980-03-10"),
+            ("15/08/2008", None, "2008-08-15"),
+            ]:
+        
+            data = [(datestr, "text")]
+            a, = _run_test_csv(header, data, date="date", text="text")
+            self.assertEqual(a.date.isoformat()[:10], expected)
